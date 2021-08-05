2ab88566e5ecdce66480da56d46ccdb3c8f4f900
Akeem C <javantieclarke@gmail.com>
2021-08-04 23:50:24 -0400

    added seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name) 
+VALUES ("IT"),
+("Marketing"),
+("HR");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer",100000,1),
+("Collaborator", 45000,2),
+("Recruiter",60000,3);
+
+INSERT INTO employee (first_name, last_name,role_id) 
+VALUES ("Geek", "Nerd", 1),
+("Famous", "Guy", 2),
+("Strict", "Person", 3),
+("eddy", "joe", 4),
+("Melissa", "Capers", 5),
+("Micheal", "Clarke", 6);
